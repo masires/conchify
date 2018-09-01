e1f85b1cae255625ddd87ecf57bdfc2bf556710e
Emanuel Simon <emanuelstorres@outlook.com>
2018-09-01 08:24:06 -0400

    added new routes, those must be reworked

--- demo/src/com/conchify/maps/android/utils/demo/GeoJsonActivity.java
+++ demo/src/com/conchify/maps/android/utils/demo/GeoJsonActivity.java
@@ -48,16 +48,57 @@ public class GeoJsonActivity extends BaseActivity implements OnMapReadyCallback 
             // https://github.com/googlemaps/android-maps-utils/blob/3b8520ed07199074e5e7124872b52b0c4f85ff90/library/tests/src/com/google/maps/android/data/geojson/GeoJsonLayerTest.java#L61
             GeoJsonLayer layerM = new GeoJsonLayer(getMap(), R.raw.route_m, this);
             layerM.getDefaultLineStringStyle().setColor(Color.parseColor("#9a12b3"));
-            layerM.getDefaultLineStringStyle().setWidth(15);
+//            layerM.getDefaultLineStringStyle().setWidth(15);
 //            assertEquals(Color.BLUE, layerM.getDefaultLineStringStyle().getColor());
 
             GeoJsonLayer layerK = new GeoJsonLayer(getMap(), R.raw.route_k, this);
             layerK.getDefaultLineStringStyle().setColor(Color.parseColor("#2b390e"));
-            layerK.getDefaultLineStringStyle().setWidth(15);
+//            layerK.getDefaultLineStringStyle().setWidth(15);
 //            assertEquals(Color.BLUE, layerM.getDefaultLineStringStyle().getColor());
+
+
+            GeoJsonLayer layerA = new GeoJsonLayer(getMap(), R.raw.route_a, this);
+            layerA.getDefaultLineStringStyle().setColor(Color.parseColor("#16a085"));
+//            layerA.getDefaultLineStringStyle().setWidth(15);
+
+
+            GeoJsonLayer layerCA = new GeoJsonLayer(getMap(), R.raw.route_ca, this);
+            layerCA.getDefaultLineStringStyle().setColor(Color.parseColor("#d47500"));
+//            layerCA.getDefaultLineStringStyle().setWidth(15);
+
+            GeoJsonLayer layerF = new GeoJsonLayer(getMap(), R.raw.route_f, this);
+            layerF.getDefaultLineStringStyle().setColor(Color.parseColor("#4f5a65"));
+//            layerF.getDefaultLineStringStyle().setWidth(15);
+
+            GeoJsonLayer layerHB = new GeoJsonLayer(getMap(), R.raw.route_hb, this);
+            layerHB.getDefaultLineStringStyle().setColor(Color.parseColor("#cf000f"));
+//            layerHB.getDefaultLineStringStyle().setWidth(15);
+
+            GeoJsonLayer layerU = new GeoJsonLayer(getMap(), R.raw.route_u, this);
+            layerU.getDefaultLineStringStyle().setColor(Color.parseColor("#96281b"));
+//            layerU.getDefaultLineStringStyle().setWidth(15);
+
+            GeoJsonLayer layerN = new GeoJsonLayer(getMap(), R.raw.route_n, this);
+            layerN.getDefaultLineStringStyle().setColor(Color.parseColor("#2e343b"));
+//            layerN.getDefaultLineStringStyle().setWidth(15);
+            GeoJsonLayer layerH = new GeoJsonLayer(getMap(), R.raw.route_h, this);
+            layerH.getDefaultLineStringStyle().setColor(Color.parseColor("#804600"));
+//            layerH.getDefaultLineStringStyle().setWidth(15);
 
             addGeoJsonLayerToMap(layerM);
             addGeoJsonLayerToMap(layerK);
+
+            addGeoJsonLayerToMap(layerA);
+            addGeoJsonLayerToMap(layerCA);
+            addGeoJsonLayerToMap(layerF);
+            addGeoJsonLayerToMap(layerHB);
+            addGeoJsonLayerToMap(layerU);
+            addGeoJsonLayerToMap(layerN);
+            addGeoJsonLayerToMap(layerH);
+
+
+
+
         } catch (IOException e) {
             Log.e(mLogTag, "GeoJSON file could not be read");
         } catch (JSONException e) {
