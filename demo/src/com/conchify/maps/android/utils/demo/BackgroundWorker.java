cf7716e914a2eadccde9b6d62214255e3a580cf4
Gabriel Rodriguez - Torres <gabrielrguez0518@gmail.com>
2018-09-06 22:00:42 -0400

    Forms and DB Connection through app completed

--- demo/src/com/conchify/maps/android/utils/demo/BackgroundWorker.java
+++ demo/src/com/conchify/maps/android/utils/demo/BackgroundWorker.java
@@ -3,6 +3,7 @@ package com.conchify.maps.android.utils.demo;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.os.AsyncTask;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -29,21 +30,33 @@ public class BackgroundWorker extends AsyncTask<String, Void, String> {
     @Override
     protected String doInBackground(String... params) {
         String type = params[0];
-        String login_url = "http://192.168.1.209/login.php";
+        String form_url = "https://contru.net/formas.php";
 
-        if (type.equals("login")) {
+        if (type.equals("form")) {
             try {
-                String user_name = params[1];
-                String password = params[2];
-                URL url = new URL(login_url);
+                String etNumero_placa_id = params[1];
+                String etNumero_carnet_id = params[2];
+                String etNumero_franja_id = params[3];
+                String etComentario = params[4];
+                String etTipo_Queja = params[5];
+                String etEsatado = params[6];
+                String etComentario_cierre = params[7];
+                String etAccion_diciplinaria = params[8];
+                URL url = new URL(form_url);
                 HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
                 httpURLConnection.setRequestMethod("POST");
                 httpURLConnection.setDoOutput(true);
                 httpURLConnection.setDoInput(true);
                 OutputStream outputStream = httpURLConnection.getOutputStream();
                 BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
-                String post_data = URLEncoder.encode("user_name", "UTF-8") + " = " + URLEncoder.encode(user_name, "UTF-8") + " & "
-                        + URLEncoder.encode("password", "UTF-8") + " = " + URLEncoder.encode(password, "UTF-8");
+                String post_data = URLEncoder.encode("numero_placa_id", "UTF-8") + "=" + URLEncoder.encode(etNumero_placa_id, "UTF-8") + "&"
+                        + URLEncoder.encode("numero_carnet_id", "UTF-8") + "=" + URLEncoder.encode(etNumero_carnet_id, "UTF-8") + "&"
+                        + URLEncoder.encode("numero_franja_id", "UTF-8") + "=" + URLEncoder.encode(etNumero_franja_id, "UTF-8") + "&"
+                        + URLEncoder.encode("comentario", "UTF-8") + "=" + URLEncoder.encode(etComentario, "UTF-8") + "&"
+                        + URLEncoder.encode("tipo_queja", "UTF-8") + "=" + URLEncoder.encode(etTipo_Queja, "UTF-8") + "&"
+                        + URLEncoder.encode("estado", "UTF-8") + "=" + URLEncoder.encode(etEsatado, "UTF-8") + "&"
+                        + URLEncoder.encode("comentario_cierre", "UTF-8") + "=" + URLEncoder.encode(etComentario_cierre, "UTF-8") + "&"
+                        + URLEncoder.encode("accion_diciplinaria", "UTF-8") + "=" + URLEncoder.encode(etAccion_diciplinaria, "UTF-8");
                 bufferedWriter.write(post_data);
                 bufferedWriter.flush();
                 bufferedWriter.close();
@@ -51,7 +64,7 @@ public class BackgroundWorker extends AsyncTask<String, Void, String> {
                 InputStream inputStream = httpURLConnection.getInputStream();
                 BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
                 String result = "";
-                String line;
+                String line = "";
                 while ((line = bufferedReader.readLine()) != null) {
                     result += line;
                 }
@@ -59,26 +72,27 @@ public class BackgroundWorker extends AsyncTask<String, Void, String> {
                 inputStream.close();
                 httpURLConnection.disconnect();
                 return result;
+
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+
         return null;
     }
 
     @Override
     protected void onPreExecute() {
         alertDialog = new AlertDialog.Builder(context).create();
-        alertDialog.setTitle("Login Status");
-
-    }
+        alertDialog.setTitle("Form Status");
+        }
 
     @Override
     protected void onPostExecute(String result) {
         alertDialog.setMessage(result);
-        alertDialog.show();
+        Toast.makeText(context, result, Toast.LENGTH_LONG).show();
     }
 
     @Override
