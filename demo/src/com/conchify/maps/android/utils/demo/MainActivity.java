9d0a644a826b06d4b21153c04fde5d9e4de449d4
Emanuel Simon <emanuelstorres@outlook.com>
2018-09-14 18:02:34 -0400

    reworked button loading, main act ui, added provisional logo

--- demo/src/com/conchify/maps/android/utils/demo/MainActivity.java
+++ demo/src/com/conchify/maps/android/utils/demo/MainActivity.java
@@ -8,38 +8,27 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-public class MainActivity extends Activity implements View.OnClickListener {
-    private ViewGroup mListView;
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        Button trayectoriaButton = findViewById(R.id.trayectoriaButton);
+        Button quejasButton = findViewById(R.id.quejasButon);
 
-        mListView = findViewById(R.id.list);
+        trayectoriaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(v.getContext(), GeoJsonActivity.class));
+            }
+        });
 
-        mapAct("Trayectoria de Conchos", GeoJsonActivity.class);
-        mapAct("Quejas de Conductores de Conchos", FormActivity.class);
-
+        quejasButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(v.getContext(), FormActivity.class));
+            }
+        });
     }
-
-    private void mapAct(String actName, Class<? extends Activity> activityClass) {
-        Button b = new Button(this);
-        ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-        b.setLayoutParams(layoutParams);
-        b.setText(actName);
-//        b.setTextColor(Color.parseColor("hex"));
-//        b.setBackgroundColor(Color.parseColor("hex"));
-        b.setTag(activityClass);
-        b.setOnClickListener(this);
-        mListView.addView(b);
-    }
-
-    @Override
-    public void onClick(View view) {
-        Class activityClass = (Class) view.getTag();
-        startActivity(new Intent(this, activityClass));
-    }
-
-
 }
