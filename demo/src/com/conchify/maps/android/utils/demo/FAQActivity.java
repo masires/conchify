722602086a553504625cadb40bf9420cd65d0590
Gabriel Rodriguez - Torres <gabrielrguez0518@gmail.com>
2018-09-22 22:46:44 -0400

    Fix FAQ and Optimized imports

--- demo/src/com/conchify/maps/android/utils/demo/FAQActivity.java
+++ demo/src/com/conchify/maps/android/utils/demo/FAQActivity.java
@@ -1,11 +1,7 @@
 package com.conchify.maps.android.utils.demo;
 
 import android.app.Activity;
-import android.graphics.Color;
-import android.graphics.Typeface;
 import android.os.Bundle;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 public class FAQActivity extends Activity {
     @Override
@@ -13,11 +9,5 @@ public class FAQActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.faq);
 
-//        String top = "Preguntas frecuentes sobre nuestra aplicación:";
-//        TextView faqview = findViewById(R.id.faqView);
-//        faqview.setText(top);
-//        faqview.setTextSize(18);
-//        faqview.setTypeface(null, Typeface.BOLD);
-//        faqview.setTextColor(Color.BLACK);
     }
 }
--- demo/src/com/conchify/maps/android/utils/demo/FormActivity.java
+++ demo/src/com/conchify/maps/android/utils/demo/FormActivity.java
@@ -27,11 +27,9 @@ public class FormActivity extends Activity {
 
     TextView tvDisplayDate;
 
-    EditText etCorreo_Electronico_id, etNumero_placa_id, etNumero_carnet_id, etNumero_franja_id, etComentario,
-            etTipo_Queja;
-//            etEstado, etComentario_cierra, etAccion_diciplinaria;
+    EditText etCorreo_Electronico_id, etNumero_placa_id, etNumero_carnet_id, etNumero_franja_id, etComentario;
 
-    String str_etCorreo_Electronico_id, str_etNumero_place_id, str_etNumero_carnet_id, str_etNumero_franja_id, str_etComentario, str_fecha, str_etTipo_Queja, str_etEstado, str_etComentario_cierra, str_etAccion_diciplinaria;
+    String str_etCorreo_Electronico_id, str_etNumero_place_id, str_etNumero_carnet_id, str_etNumero_franja_id, str_etComentario, str_fecha;
 
     Button btnSendFormInfo;
 
@@ -45,10 +43,6 @@ public class FormActivity extends Activity {
         etNumero_carnet_id = findViewById(R.id.etNumero_carnet_id);
         etNumero_franja_id = findViewById(R.id.etNumero_franja_id);
         etComentario = findViewById(R.id.etComentario);
-        etTipo_Queja = findViewById(R.id.etTipo_Queja);
-//        etEstado = findViewById(R.id.etEstado);
-//        etComentario_cierra = findViewById(R.id.etComentario_cierre);
-//        etAccion_diciplinaria = findViewById(R.id.etAccion_diciplinaria);
 
         tvDisplayDate.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -88,13 +82,7 @@ public class FormActivity extends Activity {
         str_etNumero_carnet_id = etNumero_carnet_id.getText().toString();
         str_etNumero_franja_id = etNumero_franja_id.getText().toString();
         str_etComentario = etComentario.getText().toString();
-        str_etTipo_Queja = etTipo_Queja.getText().toString();
         str_fecha = tvDisplayDate.getText().toString();
-//        tvDisplayDate = tvDisplayDate.getText().toString();
-//        str_etEstado = etEstado.getText().toString();
-//        str_etComentario_cierra = etComentario_cierra.getText().toString();
-//        str_etAccion_diciplinaria = etAccion_diciplinaria.getText().toString();
-
 
         if (str_etNumero_place_id.equals("") && str_etNumero_carnet_id.equals("") && str_etNumero_franja_id.equals("")) {
             Toast.makeText(getApplicationContext(), "Debe especificar una franja, placa o carnet para enviar una queja.", Toast.LENGTH_SHORT).show();
@@ -102,7 +90,7 @@ public class FormActivity extends Activity {
             String type = "form";
             BackgroundWorker backgroundWorker = new BackgroundWorker(this);
             backgroundWorker.execute(type, str_etCorreo_Electronico_id, str_etNumero_place_id, str_etNumero_carnet_id, str_etNumero_franja_id,
-                    str_etTipo_Queja, str_fecha, str_etComentario);
+                    str_fecha, str_etComentario);
             finish();
         }
     }
